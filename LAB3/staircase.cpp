979503d851ab44e4083a038d31d6548b26d865cd
Krunal Patil <164890358+KrunalPatil6214@users.noreply.github.com>
2026-01-11 19:19:42 +0530

    Refactor staircase drawing logic for improved clarity and structure

--- LAB3/staircase.cpp
+++ LAB3/staircase.cpp
@@ -5,63 +5,61 @@ int main() {
     cout << "Enter number of steps (n): ";
     cin >> n;
     initCanvas("3D Staircase", 800, 600);
-    double width = 300;
-    double height = 50 * n;
+    double width = 600;
     double stepHeight = 60;
 
-    double slant = 30;
     double x = (800 - width) / 2;
     double y = 550;
+
     for (int i = 0; i < n; i++) {
+        double slant = stepHeight;
 
-        double x_br = x + width;
+        double x_left = x;
+        double x_right = x + width;
+        double y_bottom = y;
         double y_top = y - stepHeight;
-        double x_tl_back = x + slant;
-        double y_back = y_top - slant;
-        double x_tr_back = x_br + slant;
 
-        double frontPolyPts[][2] = {{x, y}, {x_br, y}, {x_br, y_top}, {x, y_top}};
-        Polygon frontFace(x, y, x_br, y, x_br, y_top, x, y_top);
+        double nextWidth = width * 0.8;
+        double nextStepHeight = stepHeight * 0.8;
+        double shiftX = (width - nextWidth) / 2;
+        double next_x_left = x + shiftX;
+        double next_x_right = next_x_left + nextWidth;
+        double y_next_bottom = y_top - slant;
+
+        Rectangle frontFace(x + width/2, y - stepHeight/2, width, stepHeight);
         frontFace.setColor(COLOR("white"));
         frontFace.setFill(true);
         frontFace.imprint();
 
-        Polygon topFace(x, y_top, x_br, y_top, x_tr_back, y_back, x_tl_back, y_back);
-        topFace.setColor(COLOR("white"));
-        topFace.setFill(true);
-        topFace.imprint();
+        Line bottom(x_left, y_bottom, x_right, y_bottom);
+        Line right(x_right, y_bottom, x_right, y_top);
+        Line top(x_right, y_top, x_left, y_top);
+        Line left(x_left, y_top, x_left, y_bottom);
+        bottom.imprint(); right.imprint(); top.imprint(); left.imprint();
 
-        Line l1(x, y, x_br, y);
-        Line l2(x_br, y, x_br, y_top);
-        Line l3(x_br, y_top, x, y_top);
-        Line l4(x, y_top, x, y);
-        l1.imprint(); l2.imprint(); l3.imprint(); l4.imprint();
+        Line leftDiag(x_left, y_top, next_x_left, y_next_bottom);
+        leftDiag.imprint();
 
+        Line rightDiag(x_right, y_top, next_x_right, y_next_bottom);
+        rightDiag.imprint();
 
-        Line t1(x, y_top, x_tl_back, y_back);
-        Line t2(x_tl_back, y_back, x_tr_back, y_back);
-        Line t3(x_tr_back, y_back, x_br, y_top);
-        t1.imprint(); t2.imprint(); t3.imprint();
+        if (i == n - 1) {
+            Line backTop(next_x_left, y_next_bottom, next_x_right, y_next_bottom);
+            backTop.imprint();
+        }
 
-        int numStripes = n - i;
+        int numStripes = 4;
         double stripeGap = stepHeight / numStripes;
-
         for (int k = 1; k < numStripes; k++) {
-            double lineY = y - (k * stripeGap);
-            Line stripe(x, lineY, x_br, lineY);
+            double lineY = y_bottom - (k * stripeGap);
+            Line stripe(x_left, lineY, x_right, lineY);
             stripe.imprint();
         }
 
-        double nextWidth = width * 0.8;
-        double nextHeight = stepHeight * 0.8;
-
-        double shiftX = (width - nextWidth) / 2;
-        x = x + shiftX;
-
-        y = y - stepHeight;
-
+        y = y_next_bottom;
+        x = next_x_left;
         width = nextWidth;
-        stepHeight = nextHeight;
+        stepHeight = nextStepHeight;
     }
 
     getClick();
